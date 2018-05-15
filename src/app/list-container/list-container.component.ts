9915e2bf1dd57b3b0706b533f56689c7ee5ab93a
Carlos Díaz <carlosberrio08@hotmail.com>
2018-05-14 23:16:45 -0500

    Add/Remove from Favorites feature added.

--- src/app/list-container/list-container.component.ts
+++ src/app/list-container/list-container.component.ts
@@ -36,20 +36,12 @@ export class ListContainerComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges(changes: { [propKey: string]: SimpleChange }) {
-    this.movieService.searchMovie(this.query).then(data => {
-      this.moviesSearch = data;
-    });
+    if (this.query !== '') {
+      this.movieService.searchMovie(this.query).then(data => {
+        this.moviesSearch = data;
+      });
+    }
   }
 
-  /*searchMovie() {
-    if (this.query !== '') {
-      this.http.get('https://api.themoviedb.org/3/search/movie?api_key=' + this.APIKEY + '&query=' + this.query).toPromise()
-        .then(result => result)
-        .then((data: any) => {
-          this.moviesSearch = data.results;
-          console.log(this.moviesSearch);
-        });
-    }
-  }*/
 
 }
